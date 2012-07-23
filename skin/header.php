6e43500b8f04f0935ba71358ece66742f7bdf765
Denis Hoctor <denishoctor@gmail.com>
2012-07-23 20:57:57 +1000

    Adding CSS cache busting

--- skin/header.php
+++ skin/header.php
@@ -25,7 +25,7 @@
 
 	<meta name="viewport" content="width=940">
 
-	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/style.css?v2.0.1">
 	
 	<script src="/js/libs/modernizr-2.5.3-respond-1.1.0.min.js"></script>
 </head>
